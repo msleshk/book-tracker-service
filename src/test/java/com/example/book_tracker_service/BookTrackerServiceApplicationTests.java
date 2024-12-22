afec486ada9c18ac127f8d99a43c941f3ca962aa
Maksim Leshko <msleshko2003@gmail.com>
2024-12-22 13:03:01 +0300

    Initial commit

---
+++ src/test/java/com/example/book_tracker_service/BookTrackerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.book_tracker_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookTrackerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
